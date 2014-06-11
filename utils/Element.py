713e334457ccd7f3f699a8a282e343e35dda0a53
Dean Silfen <dean.silfen@gmail.com>
2014-06-10 21:42:17 -0400

    added positioning for author

--- utils/Element.py
+++ utils/Element.py
@@ -11,7 +11,7 @@ import os
 class Element(object):
 
     def __init__(self, filename=None, text='', 
-            duration=10, author=None):
+            duration=10, author=''):
         self.filename = filename
         self.text = text
         self.author = author
@@ -20,19 +20,11 @@ class Element(object):
             self.blurred = str(uuid.uuid4()) + '.png'
             self.rectangle = str(uuid.uuid4()) + '.png'
             self.video = str(uuid.uuid4()) + '.mp4'
+            self.width=800
+            self.height=400
         else:
             raise NameError('File is not .jpg or .png!')
     
-    def format_content(self):
-        '''
-        There should be logic to control the quote.
-        Make sure quote is not too long, make sure the rectangle
-        will fit the quote
-        '''
-        self.width=800
-        self.height=400
-        pass
-
     def create_rectangle(self):
         with Color('white') as color:
             with Image(width=self.width, height=self.height, background=color) as image:
@@ -48,9 +40,11 @@ class Element(object):
     def merge(self):    
         background = moviepy.editor.ImageClip(self.blurred)
         rectangle = moviepy.editor.ImageClip(self.rectangle)
-        text = moviepy.editor.TextClip(self.text, fontsize=70, color='black')
+        text = moviepy.editor.TextClip(self.text,fontsize=70, font='Century-Schoolbook-L-Roman')
+        author = moviepy.editor.TextClip(self.author, fontsize=18, font='Century-Schoolbook-L-Roman')
         video = moviepy.editor.CompositeVideoClip([
             background.set_duration(self.duration),
             rectangle.set_pos('center').set_duration(self.duration),
-            text.set_pos('center').set_duration(self.duration)])
+            text.set_pos('center').set_duration(self.duration),
+            author.set_pos((720,500)).set_duration(self.duration)])
         video.to_videofile(self.video, fps=60)
--- utils/tests/test_Element.py
+++ utils/tests/test_Element.py
@@ -1,18 +1,16 @@
 from utils.Element import Element
-from utils.CreateMedia import CreateMedia
 
 import pytest
 
 import os
 
 def test_elements():
-    test = Element(filename='test.jpg', text='Hello World')
+    test = Element(filename='test.jpg', text='Hello World', author='Dean Rex', duration=1)
     before = os.listdir('.')
-    test.format_content()
     test.create_background()
     test.create_rectangle()
     test.merge()
     assert len(os.listdir('.'))-len(before)==3
     with pytest.raises(NameError) as execinfo:
         bad = Element(filename='bad_filename')
-
+    os.system('rm *png')
