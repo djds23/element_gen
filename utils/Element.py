f279ad5f51b6958c1de5bf12bda424eee1fc73f8
Dean Silfen <dean.silfen@gmail.com>
2014-06-17 14:59:38 -0400

    working on dynamically adjusting word placement for the text

--- utils/Element.py
+++ utils/Element.py
@@ -5,29 +5,33 @@ from wand.drawing import Drawing
 import moviepy.editor
 
 import uuid
-
-import os 
+import math
+import os
 
 class Element(object):
+    '''Construct the video, all resolutions set to be 1280x720'''
 
-    def __init__(self, filename=None, text='', 
+    def __init__(self, filename=None, quote='',
             duration=10, author=''):
         self.filename = filename
-        self.text = text
+        self.quote = quote
         self.author = author
         self.duration =duration
         if self.filename.endswith('.jpg' or '.png'):
             self.blurred = str(uuid.uuid4()) + '.png'
             self.rectangle = str(uuid.uuid4()) + '.png'
             self.video = str(uuid.uuid4()) + '.mp4'
-            self.width=800
-            self.height=400
+            self.width = 1280
+            self.height = 720
+            self.inner_width=906
+            self.inner_height=560
         else:
             raise NameError('File is not .jpg or .png!')
-    
+
     def create_rectangle(self):
         with Color('white') as color:
-            with Image(width=self.width, height=self.height, background=color) as image:
+            with Image(width=self.inner_width, height=self.inner_height,
+                    background=color) as image:
                 image.save(filename=self.rectangle)
 
     def create_background(self):
@@ -37,14 +41,26 @@ class Element(object):
                 blurred.gaussian_blur(0,6)
                 blurred.save(filename=self.blurred)
 
-    def merge(self):    
+    def size_rectangle(self):
+        '''Determine the size of inner rectangle'''
+        ratio = 1.61803398875
+        quote = '\"' + self.quote + '\"'
+        character_length=len(quote)
+        area_per_char = character_length/ratio
+        self.font_size = int(math.sqrt(area_per_char))
+        self.start_width = (self.width-self.inner_width) + (self.font_size+1)
+        self.start_height = (self.height-self.inner_height) + (self.font_size+1)
+
+    def merge(self):
         background = moviepy.editor.ImageClip(self.blurred)
         rectangle = moviepy.editor.ImageClip(self.rectangle)
-        text = moviepy.editor.TextClip(self.text,fontsize=70, font='Century-Schoolbook-L-Roman')
-        author = moviepy.editor.TextClip(self.author, fontsize=18, font='Century-Schoolbook-L-Roman')
+        quote = moviepy.editor.TextClip(self.quote,fontsize=self.font_size,
+                font='Century-Schoolbook-L-Roman')
+        author = moviepy.editor.TextClip(self.author, fontsize=self.font_size,
+                font='Century-Schoolbook-L-Roman')
         video = moviepy.editor.CompositeVideoClip([
             background.set_duration(self.duration),
             rectangle.set_pos('center').set_duration(self.duration),
-            text.set_pos('center').set_duration(self.duration),
+            quote.set_pos((self.start_width, self.start_height)).set_duration(self.duration),
             author.set_pos((720,500)).set_duration(self.duration)])
         video.to_videofile(self.video, fps=60)
