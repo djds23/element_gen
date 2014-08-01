524a2b4b1b3b576b303130807f6882db41317e53
Dean Silfen <dean.silfen@gmail.com>
2014-07-31 23:58:47 -0400

    got the bare bones look going, will conquer more and integrate this tomorrow, its looking like the Wand Library and original Element module are HISTORY

--- element_gen/Twitter_Element.py
+++ element_gen/Twitter_Element.py
@@ -1,25 +1,26 @@
+import textwrap
 import uuid
-import math
 import os
-import re
 
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
 
 import moviepy.editor
-
-from .Element import Element
 
 from urllib import urlopen, urlretrieve, urlcleanup
 
 from bs4 import BeautifulSoup
 
-class Twitter_Element(Element):
+CONFIGURE = {
+        'FONTPATH': '/usr/share/fonts/truetype/freefont/FreeSans.ttf'
+        }
+
+class Twitter_Element(object):
 
     def __init__(self, username):
         self.username = username
         self.get_tweets(self.username)
         self.tile_photo()
-        self.tweet_canvas()
+        self.tweet_canvas(self.tweets[0])
         self.filename = str(uuid.uuid4())+'.jpg'
         self.canvas.save(self.filename)
 
@@ -54,8 +55,24 @@ class Twitter_Element(Element):
             start_height += self.i_file.size[1]
         self.canvas = canvas.filter(ImageFilter.GaussianBlur(12))
 
-    def tweet_canvas(self):
+    def tweet_canvas(self, tweet):
         tweet_canvas_size = (3*self.i_file.size[0], self.i_file.size[1])
         tweet_canvas = Image.new('RGB', tweet_canvas_size, 'white')
         tweet_canvas.paste(self.i_file, (0,0))
+        draw = ImageDraw.Draw(tweet_canvas)
+        font = ImageFont.truetype(CONFIGURE['FONTPATH'], 16, 0)
+        self.prep_tweet(tweet, draw, font)
+        draw.text((1000, 340), '@' + self.username, (0,0,0), font)
         self.canvas.paste(tweet_canvas, (40,160))
+
+    def prep_tweet(self, tweet, draw, font=None):
+        tweet = tweet.encode('ascii', 'ignore')
+        tweet = textwrap.wrap(tweet, 70)
+        max_height = 80
+        max_width = 1600
+        color = (0,0,0)
+        full_line = ''
+        for line in tweet:
+            width, height = draw.textsize(line, font)
+            draw.text(((max_width-width)/2, max_height), line, color, font)
+            max_height += 40
