9e012bc81e9c0eee61d9336fc177dbc1559c99a9
Dean Silfen <dean.silfen@gmail.com>
2014-08-02 15:44:49 -0400

    adjust setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
-from distutils.core import setup
+from distutils.core import setup, Command
+
+class PyTest(Command):
+    user_options=[]
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import sys, subprocess
+        errno = subprocess.call([sys.executable, 'runtest.py'])
+        raise SystemExit(errno)
 
 setup(
+    cmd = {'test':PyTest},
     name='element_gen',
     version='0.1',
     author='Dean Rex Silfen',
